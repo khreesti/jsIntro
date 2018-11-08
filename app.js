eea65a7d99086f55399c586701f8381bbbf417d4
Kristy Ong <kristy.mil.ong@gmail.com>
2018-11-07 17:23:42 -0700

    added Javascript intro

---
+++ app.js
@@ -0,0 +1,118 @@
+    var sum = 1 + 2
+
+    // alert(sum) 
+    
+    /*
+    this is a 
+    multi line comment
+    */
+
+    var diff = 1-2
+    var product = 1 * 2
+    var div = 1 / 2
+    
+    
+    //order of operation
+    //console.log(1 + 2 * 3 + (9-2))
+    //console.log(diff) is the same as console.log( 1 - 2)
+    //console.log(product)
+    //console.log(div)
+
+    var string ="hello"
+    var string2 = "world"
+
+    // "hello" + "world" equals "hello world"
+    // adding two strings, term is concatination, resulting in a larger string
+    //console.log(string + string2)
+
+    //console.log(string + 1)
+    // = assignment
+    // == equality
+
+
+    // integer whole number 1, 3, 17
+
+    // float decimal numbers 1.2, 3.14, 12.0
+
+    // boolean  true, false               1 0
+
+    // arrays collection of objects 
+    // an array sits in []
+    // [1, 4, 5, 2]
+    // ["hi", "hello", "hey"]
+    // [1, "hi", 1.3, true]
+
+    // function 
+
+    function sayHello() {
+      console.log('hello')
+    }
+
+    // sayHello()
+
+    var num1 = 2
+    var num2 = 12
+
+    function product(x, y) {
+      console.log(x * y)
+    }
+
+    //product(num1, num2)
+
+    function btnClick() {
+      alert('Yay you clicked me')
+    }
+
+    function mouseOver() {
+      console.log('hi')
+    }
+
+    var h = 3
+    // this is called reassignment    h is now 11
+    h = 1
+    h = 7
+    h = 9
+    h = 10
+    h = 11
+
+    l = 1
+
+    // console.log(l)
+    // console.log(1 == true)
+
+    //console.log('hello' == 1)
+    
+    //console.log('1' == 1)
+
+    // array
+    var arr = [1, 2, 3, 4, 5]
+    //         0  1  2  3  4
+    // position of arrays are called indexes, indexes are 0 based
+
+    //console.log(arr[2])
+    // this would display the number 3
+
+    //var myArr = [1, 'hi', 1.2, true, 23]
+    //console.log(myArr[3])
+
+    //var mySuperArr = [ [1,3], ["ae", "we"], [1.23, true] ]
+    //console.log(mySuperArr[2][1])
+
+
+    //console.log(arr.shift())
+
+    // how much the array can hold is the length
+    //console.log(arr.length)
+
+    // push adds an item onto the array
+    console.log(arr.push(2))
+    console.log(arr)
+
+    // remove the last element from the array
+    console.log(arr.pop())
+    console.log(arr)
+
+
+   /* function product() {
+
+    }*/
